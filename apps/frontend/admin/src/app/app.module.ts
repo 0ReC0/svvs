4f9ea36f38f9a2be1ea91025436c7b258c9f332e
Kitanin Sergei <kitanin.ser@mail.ru>
2022-01-21 17:19:12 +0300

    feat(IDE): delete default html app.component, disable eslint
    * delete default html app.component,
    * disable eslint
    
    Closes #2

--- apps/frontend/admin/src/app/app.module.ts
+++ apps/frontend/admin/src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
-import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component'
+import { NxWelcomeComponent } from './nx-welcome.component'
+import { RouterModule } from '@angular/router'
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
@@ -14,4 +14,5 @@ import { RouterModule } from '@angular/router';
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
